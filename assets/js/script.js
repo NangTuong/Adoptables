fe18de112ad1fee269033fa477e59a46f2c57bb1
joe-white09 <joe_white09@yahoo.com>
2022-05-30 14:00:21 -0500

    dog fact api added to js

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+var factSpan = document.querySelector("#dog-fact");
+var factBtn = document.querySelector("#fact-btn");
+
+var dogFacts = [];
+
+var getDogFact = function() {
+    var dogFactApiUrl = "https://cors-anywhere.herokuapp.com/https://dog-api.kinduff.com/api/facts?number=100";
+    fetch(dogFactApiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {                
+                dogFacts.push(data);
+                console.log(dogFacts);
+            })
+        }
+    })
+};
+
+var displayFact = function() {
+    var fact = dogFacts[0].facts[Math.floor(Math.random() * (dogFacts[0].facts).length)];
+    console.log(fact);
+};
+
+factBtn.addEventListener("click", displayFact);
+getDogFact();
