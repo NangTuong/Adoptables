d11030ba7c7cc8c84b5373ec9c8204a6942d37e8
Taylor Leverette <kyle_taylor315@hotmail.com>
2022-05-31 20:57:15 -0500

    added token and petfinder API calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var factBtn = document.querySelector("#fact-btn");
 var dogFacts = [];
 
 var getDogFact = function() {
-    var dogFactApiUrl = "https://cors-anywhere.herokuapp.com/https://dog-api.kinduff.com/api/facts?number=100";
+    var dogFactApiUrl = "https://cors-anywhere.herokuapp.com/https://dog-api.kinduff.com/api/facts?number=10";
     fetch(dogFactApiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {                
@@ -12,6 +12,9 @@ var getDogFact = function() {
                 console.log(dogFacts);
             })
         }
+    })
+    .catch(function(error){
+        factSpan.textContent="Dog facts unavailble at this time!";
     })
 };
 
@@ -21,5 +24,46 @@ var displayFact = function() {
     factSpan.textContent= fact;
 };
 
+
+
+var getToken = function(){
+    var url = "https://api.petfinder.com/v2/oauth2/token";
+    fetch(url, {
+        method:"post",
+        body: "grant_type=client_credentials&client_id=vQGWniAcJTVR7FPzWWR77YyrmNnOaZ5RtzoEAnLGBmNwBEGKpo&client_secret=uSRpQnsKOJbvtEkr9LdteF1izemRN9CG6yhnzNFT",
+        headers: new Headers({
+            "Content-Type": "application/x-www-form-urlencoded"
+        })
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        getDog(data.access_token);
+    })
+};
+
+var getDog = function(token){
+    var url = "https://api.petfinder.com/v2/animals?type=dog";
+    fetch(url, {
+        method:"get",
+        headers: new Headers({
+            "Authorization": "Bearer " + token
+        })
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    })
+    // add local storage
+}
+
+getToken();
+
+
 factBtn.addEventListener("click", displayFact);
 getDogFact();
+
